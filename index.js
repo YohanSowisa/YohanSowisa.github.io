d194534a176b37e275b58b635ac37ac06e58347d
YohanSowisa <sowisayohanes@gmail.com>
2024-01-14 11:26:00 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let body = document.querySelector('body')
+let input = document.querySelector('input')
+let btnChange = document.querySelector('#change')
+let btnDef = document.querySelector('#def')
+
+btnChange.onclick = function() {
+  body.style.background = input.value
+
+  const split = new Array(...input.value)
+  if (split.filter(Number)) {
+    body.style.background = `#${input.value}`
+  }
+
+  input.value = ''
+}
+btnDef.onclick = function() {
+  body.style.background = 'initial'
+}
